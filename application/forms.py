a21ee42393291f64329289d84fbe806c0b15f618
Lisa P <lisajp4@gmail.com>
2021-06-26 00:23:30 +0000

    Updating Friday's work

--- application/forms.py
+++ application/forms.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField
+from wtforms import StringField, SubmitField, SelectField, IntegerField
 
 class New(FlaskForm):
+    report_id = IntegerField('Report Number')
     product_section = SelectField('Section', choices=[(1, "Outdoor Plants"), (2, "Indoor Plants"), (3, "Outdoor Paints"), (4, "Gardening Equipment")])
     report_type = SelectField('Type', choices=[("Damage"), ("Theft"), ("Injury"), ("Complaint"), ("Training"), ("Illness"), ("Technical")])
     description = StringField('Description')
     resolution = StringField('Resolution')
     complete = StringField('Complete')
     submit = SubmitField('Submit Report')
+
--- application/routes.py
+++ application/routes.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 from application.models import Types, Reports
 from application.forms import New
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField
+from wtforms import StringField, SubmitField, SelectField, IntegerField
 from os import getenv
 
 @app.route('/')
@@ -16,7 +16,7 @@ def readreports():
     reports_string = ""
     for issues in all_reports:
         reports_string += "<br>"+ "<br>"+ str(issues.report_id) + "   |   " + str(issues.incident_date) + "   |   " +  issues.report_type + "   |   " + issues.description + "  |  " +  issues.complete + "  |  " + str(issues.resolution) + "   |   "
-    return render_template('current_issues.html') + reports_string
+    return render_template('readreports.html') + reports_string
 
 @app.route('/newreport', methods=['GET', 'POST'])
 def newreport():
@@ -27,11 +27,8 @@ def newreport():
         nreport = Reports(product_section = form.product_section.data, description = form.description.data, resolution = form.resolution.data, complete = form.complete.data, report_type = form.report_type.data)
         db.session.add(nreport)
         db.session.commit()
-
     return render_template("newreport.html", form=form)
-
-
-
+    
 @app.route('/policies')
 def policies():
     all_policies = Types.query.all()
@@ -39,3 +36,29 @@ def policies():
     for policy in all_policies:
         policies_string += "<br>"+ "<br>"+ policy.type_id + "   |   "  + policy.policies + "  |  "
     return render_template('policies.html') + policies_string
+
+#@app.route('/updatereport', methods=['GET', 'POST'])
+#def updatereport():
+#    error = ""
+#    form = New()
+#
+#    if request.method == 'POST':
+#        find_report = Reports.query.filter_by(report_id=form.report_id.data)
+#        for change in find_report:
+#            find_report.resolution = form.resolution.data
+#            find_report.complete = form.complete.data
+#            db.session.commit()
+#    return render_template("updates.html", form=form)
+
+@app.route('/resolve/', methods=['GET', 'POST'])
+def resolve():
+    error = ""
+    form = New()
+
+    if request.method == 'POST':
+        find_report = Reports.query.get(form.report_id.data)
+        find_report.resolution = form.resolution.data
+        find_report.complete = form.complete.data
+        db.session.commit()
+    return render_template("resolve.html", form=form)
+    
